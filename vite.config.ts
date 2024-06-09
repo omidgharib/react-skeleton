0d14a5890d662ea2376f7f99638f0e76b83b01d4
Omid Gharib <omidgharib@yahoo.com>
2024-06-09 22:10:13 +0330

    fixed an issue tailwind.config format

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,44 @@
 // import react from "@vitejs/plugin-react";
-import react from "@vitejs/plugin-react-swc";
-import * as path from "path";
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import vitePluginImp from "vite-plugin-imp";
+import react from '@vitejs/plugin-react-swc'
+import * as path from 'path'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vitePluginImp from 'vite-plugin-imp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        vitePluginImp({
-            libList: [
-                {
-                    libName: "antd",
-                    //     style: (name) => {
-                    //         if (name === "col" || name === "row") {
-                    //             return "antd/lib/style/index.less";
-                    //         }
-                    //         return `antd/es/${name}/style/index.less`;
-                    //     },
-                },
-            ],
-        }),
-    ],
-    css: {
-        preprocessorOptions: {
-            less: {
-                javascriptEnabled: true,
-            },
+  plugins: [
+    react(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          //     style: (name) => {
+          //         if (name === "col" || name === "row") {
+          //             return "antd/lib/style/index.less";
+          //         }
+          //         return `antd/es/${name}/style/index.less`;
+          //     },
         },
+      ],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
     },
-    define: { "process.env": process.env },
+  },
+  define: { 'process.env': process.env },
 
-    optimizeDeps: {
-        include: ["tailwind.config.js"],
+  optimizeDeps: {
+    include: ['tailwind.config.ts'],
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      'tailwind.config.ts': resolve(__dirname, 'tailwind.config.ts'),
     },
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "./src"),
-            "tailwind.config.js": resolve(__dirname, "tailwind.config.js"),
-        },
-    },
-});
+  },
+})
